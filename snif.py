82a7a5a362f71936340066b7a75a5f660d0549bd
ahangchen <cweihang@foxmail.com>
2018-05-12 05:31:03 +0000

    upload packet timestamp

--- snif.py
+++ snif.py
@@ -3,24 +3,27 @@ import os, traceback, sys
 import argparse
 import warnings
 import json
-from scapy.all import Dot11ProbeReq, Dot11,sniff, Dot11ProbeResp
+from scapy.all import Dot11ProbeReq, Dot11,sniff, Dot11ProbeResp, TCP
 import manuf
 from backend.sensor import upload_wifi_info
 
 def monitorDevice(pkt):
     addr, target = None, None
+    time_stamp = -1
     if pkt.haslayer(Dot11ProbeReq):
         addr = pkt.getlayer(Dot11).addr2
         target = pkt.getlayer(Dot11ProbeReq).info or ''
+        time_stamp = pkt.getlayer(Dot11ProbeReq).time
     if pkt.haslayer(Dot11ProbeResp):
         addr = pkt.getlayer(Dot11).addr1
         target = pkt.getlayer(Dot11ProbeResp).info or ''
+        time_stamp = pkt.getlayer(Dot11ProbeResp).time
     if addr and target:
         manuf = parser.get_manuf(addr) or 'Unknown'
         rssi = -(256-ord(pkt.notdecoded[-4:-3]))
         try:
             print('Detected Devices: MAC[%s] Manuf[%s] Target[%s] RSSI [%d]' % (addr, manuf, target, rssi))
-            upload_wifi_info(addr, rssi, int(conf['sensorId']))
+            upload_wifi_info(addr, rssi, int(conf['sensorId']), time_stamp)
             # print('Detected Devices: MAC[%s] Target[%s] RSSI [%d]' % (addr, target, rssi))
         except Exception, err:
             traceback.print_exc()
